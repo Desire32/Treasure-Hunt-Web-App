8ff186d35517b879bd480797c113466b310e11fe
Desire32 <nmarkov@uclan.ac.uk>
2024-03-20 19:08:08 +0200

    Small fixes of button start.html, question.html page, startButton index.html

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('buttonStart').addEventListener('click', function()
+{
+   window.location.href = 'list.html';
+});
--- start.js
+++ start.js
@@ -1,35 +1,31 @@
-let params = new URLSearchParams(location.search)
-let treasureHuntID = params.get('uuid')
-console.log(treasureHuntID)
+document.getElementById('playerName').addEventListener('input', function () {
+	let playerName = this.value.trim()
+	let nameButton = document.getElementById('nameButton')
+	if (playerName !== '') {
+    document.getElementById('nameButton').addEventListener('click', function () {
+				window.location.href = 'question.html'
+			})
+		nameButton.disabled = false
+	} else {
+		nameButton.disabled = true
+	}
+})
 
+function addWordToStorage() {
+	let word = document.getElementById('playerName').value.trim()
 
-let errorMessagesStartElement = document.getElementById('errorMessages') //doesnt work for now, needs to read start json file
-fetch('https://codecyprus.org/th/api/start')
-	.then(response => response.json())
-	.then(jsonObject => {
-		let html = ''
-		for (let i = 0; i < jsonObject.errorMessages.length; i++) {
-			html += `<li class="errorMessagesSize" onclick="handleClick('${jsonObject.errorMessages[i].uuid}')">${jsonObject.errorMessages[i].name}</li>`
+	if (word !== '') {
+		let storedWords = JSON.parse(localStorage.getItem('storedWords')) || []
+		if (storedWords.includes(word)) {
+			alert('Such name has already been used before, try again')
+			return
 		}
-		errorMessagesStartElement.innerHTML = html;
-	})
 
+		storedWords.push(word)
+		localStorage.setItem('storedWords', JSON.stringify(storedWords))
 
-  function addWordToStorage()
-  {
-    let word = document.getElementById('playerName').value;
+		document.getElementById('playerName').value = ''
 
-    if(word.trim() !== ''){
-
-      let storedWords= JSON.parse(localStorage.getItem('storedWords')) || [];
-
-      storedWords.push(word);
-
-      localStorage.setItem('storedWords', JSON.stringify(storedWords));
-
-
-      document.getElementById('playerName').value = '';
-
-      console.log('Stored words: ', storedWords); //testing
-    } 
-  }
+		console.log('Stored words:', storedWords) //testing
+	}
+}
