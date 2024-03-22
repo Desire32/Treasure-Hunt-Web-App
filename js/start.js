0bd2b4e645e828651bd5692e715dc21619d2478a
Desire32 <nmarkov@uclan.ac.uk>
2024-03-22 16:40:38 +0200

    js changes of reading api

--- js/start.js
+++ js/start.js
@@ -1,57 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
-	let testNameElement = document.getElementById('testInfo')
-	let playerNameInput = document.getElementById('playerName')
-	let nameButton = document.getElementById('nameButton')
-	let params = new URLSearchParams(window.location.search)
-	let uuid = params.get('uuid')
-	console.log(uuid)
+let testNameElement = document.getElementById('testInfo')
+let playerNameInput = document.getElementById('playerName')
+let nameButton = document.getElementById('nameButton')
+let uuid = localStorage.getItem('uuid')
 
-	nameButton.addEventListener('click', function () {
-		let playerName = playerNameInput.value.trim()
-		if (playerName !== '') {
-			let url =
-				'https://codecyprus.org/th/api/start' +
-				'?player=' +
-				playerName +
-				'&app=simpsons-app' +
-				'&treasure-hunt-id=' +
-				uuid
+function start() {
 
-			fetch(url)
-				.then(response => response.json())
-				.then(jsonObject => {
-					console.log(jsonObject)
-					if (jsonObject.status === 'OK') {
-						let session = jsonObject.session
-						let numOfQuestions = jsonObject.numOfQuestions
-					} else {
-					}
-				})
-				addWordToStorage();
-		} else {
-			alert('Please enter your name')
-		}
-	})
+	let playerName = playerNameInput.value.trim()
+	if (playerName !== '') {
+		let url =
+			'https://codecyprus.org/th/api/start' + '?player=' + playerName + '&app=ComputerLegendsApp' + '&treasure-hunt-id=' + uuid
 
-	function addWordToStorage() {
-		let word = playerNameInput.value.trim()
-		if (word !== '') {
-			try {
-				let storedWords = JSON.parse(localStorage.getItem('storedWords')) || []
-				if (storedWords.includes(word)) {
-					alert('Such name has already been used before, try again')
-					return
+		fetch(url)
+			.then(response => response.json())
+			.then(jsonObject => {
+				console.log(jsonObject)
+				if (jsonObject.status === 'OK') {
+					let session = jsonObject.session
+					localStorage.setItem('sessionID', session)
+					let numOfQuestions = jsonObject.numOfQuestions
+					localStorage.setItem('numOfQuestions', numOfQuestions)
 				}
+			})
+		addWordToStorage()
+	} else {
+		alert('Please enter your name')
+	}
+}
 
-				storedWords.push(word)
-				localStorage.setItem('storedWords', JSON.stringify(storedWords))
-				playerNameInput.value = ''
-
-				let lastWordArray = storedWords[storedWords.length - 1]
-				testElements.innerHTML += `<li>Name: ${lastWordArray}</li>`
-			} catch (error) {
-				console.error('Reading error')
+function addWordToStorage() {
+	let word = playerNameInput.value.trim()
+	if (word !== '') {
+		try {
+			let storedWords = JSON.parse(localStorage.getItem('storedWords')) || []
+			if (storedWords.includes(word)) {
+				alert('Such name has already been used before, try again')
+				return
 			}
+			storedWords.push(word)
+			localStorage.setItem('storedWords', JSON.stringify(storedWords))
+			playerNameInput.value = ''
+
+			let lastWordArray = storedWords[storedWords.length - 1]
+			testElements.innerHTML += `<li>Name: ${lastWordArray}</li>`
+		} catch (error) {
+			console.error('Reading error')
 		}
 	}
-})
+}
