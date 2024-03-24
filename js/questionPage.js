cab427dfe16e45f6f5676f5be0513d8b4f7eeff4
Desire32 <nmarkov@uclan.ac.uk>
2024-03-24 13:22:22 +0200

    small fix skip api and question container

--- js/questionPage.js
+++ js/questionPage.js
@@ -17,38 +17,66 @@ function getQuestion(sessionID) {
 
 function generateQuestionHTML(jsonObject) {
 	let html = ''
+	html += `<div class="questionContainer">`
+	html += `<h3>Question ${jsonObject.currentQuestionIndex + 1}</h3>`
 	switch (jsonObject.questionType) {
 		case 'BOOLEAN':
 			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
 			html += `<li class="PersonInfoPanel">Please select true or false:</li>`
 			html += `<button class="PersonInfoPanel trueButton">True</button>`
 			html += `<button class="PersonInfoPanel falseButton">False</button>`
-			html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			if (jsonObject.canBeSkipped) {
+				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			}
 			break
 		case 'INTEGER':
-		case 'NUMERIC':
-		case 'TEXT':
 			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
-			html += `<li class="PersonInfoPanel">Please enter a${
-				jsonObject.questionType === 'INTEGER' ? 'n integer' : ' '
-			} answer:</li>`
-			html += `<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>`
-			html += `<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />`
-			html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			html += `<li class="PersonInfoPanel">Please enter an integer answer:</li>`
+			html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
+			html +=
+				'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
+			if (jsonObject.canBeSkipped) {
+				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			}
+			break
+		case 'NUMERIC':
+			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+			html += `<li class="PersonInfoPanel">Please enter a numeric answer:</li>`
+			html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
+			html +=
+				'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
+			if (jsonObject.canBeSkipped) {
+				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			}
 			break
 		case 'MCQ':
 			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
 			html += `<li class="PersonInfoPanel">Please select one of the following options:</li>`
-			for (let option of jsonObject.options) {
-				html += `<button class="PersonInfoPanel mcqOption" value="${option}">${option}</button>`
+			html += `<button class="PersonInfoPanel mcqOption" value="A">A</button>`
+			html += `<button class="PersonInfoPanel mcqOption" value="B">B</button>`
+			html += `<button class="PersonInfoPanel mcqOption" value="C">C</button>`
+			html += `<button class="PersonInfoPanel mcqOption" value="D">D</button>`
+			if (jsonObject.canBeSkipped) {
+				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
 			}
-			html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			break
+		case 'TEXT':
+			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+			html += `<li class="PersonInfoPanel">Please enter a single word answer:</li>`
+			html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
+			html +=
+				'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
+			if (jsonObject.canBeSkipped) {
+				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+			}
 			break
 		default:
 			break
 	}
+	html += `</div>`
 	return html
 }
+
 
 document.addEventListener('click', function (event) {
 	let sessionID = localStorage.getItem('sessionID')
