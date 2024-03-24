df0a84a99950ef82a7513b7349186651f20fcdc2
Desire32 <nmarkov@uclan.ac.uk>
2024-03-24 13:58:57 +0200

    correct skip button and deleting extra question

--- js/questionPage.js
+++ js/questionPage.js
@@ -16,65 +16,67 @@ function getQuestion(sessionID) {
 }
 
 function generateQuestionHTML(jsonObject) {
-	let html = ''
-	html += `<div class="questionContainer">`
-	html += `<h3>Question ${jsonObject.currentQuestionIndex + 1}</h3>`
-	switch (jsonObject.questionType) {
-		case 'BOOLEAN':
-			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
-			html += `<li class="PersonInfoPanel">Please select true or false:</li>`
-			html += `<button class="PersonInfoPanel trueButton">True</button>`
-			html += `<button class="PersonInfoPanel falseButton">False</button>`
-			if (jsonObject.canBeSkipped) {
-				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
-			}
-			break
-		case 'INTEGER':
-			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
-			html += `<li class="PersonInfoPanel">Please enter an integer answer:</li>`
-			html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
-			html +=
-				'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
-			if (jsonObject.canBeSkipped) {
-				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
-			}
-			break
-		case 'NUMERIC':
-			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
-			html += `<li class="PersonInfoPanel">Please enter a numeric answer:</li>`
-			html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
-			html +=
-				'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
-			if (jsonObject.canBeSkipped) {
-				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
-			}
-			break
-		case 'MCQ':
-			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
-			html += `<li class="PersonInfoPanel">Please select one of the following options:</li>`
-			html += `<button class="PersonInfoPanel mcqOption" value="A">A</button>`
-			html += `<button class="PersonInfoPanel mcqOption" value="B">B</button>`
-			html += `<button class="PersonInfoPanel mcqOption" value="C">C</button>`
-			html += `<button class="PersonInfoPanel mcqOption" value="D">D</button>`
-			if (jsonObject.canBeSkipped) {
-				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
-			}
-			break
-		case 'TEXT':
-			html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
-			html += `<li class="PersonInfoPanel">Please enter a single word answer:</li>`
-			html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
-			html +=
-				'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
-			if (jsonObject.canBeSkipped) {
-				html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
-			}
-			break
-		default:
-			break
+	let html = '';
+	html += `<div class="questionContainer">`;
+	if (jsonObject.currentQuestionIndex < 6) {
+		html += `<h3>Question ${jsonObject.currentQuestionIndex + 1}</h3>`;
+		switch (jsonObject.questionType) {
+			case 'BOOLEAN':
+				html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+				html += `<li class="PersonInfoPanel">Please select true or false:</li>`
+				html += `<button class="PersonInfoPanel trueButton">True</button>`
+				html += `<button class="PersonInfoPanel falseButton">False</button>`
+				if (jsonObject.canBeSkipped) {
+					html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+				}
+				break
+			case 'INTEGER':
+				html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+				html += `<li class="PersonInfoPanel">Please enter an integer answer:</li>`
+				html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
+				html +=
+					'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
+				if (jsonObject.canBeSkipped) {
+					html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+				}
+				break
+			case 'NUMERIC':
+				html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+				html += `<li class="PersonInfoPanel">Please enter a numeric answer:</li>`
+				html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
+				html +=
+					'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
+				if (jsonObject.canBeSkipped) {
+					html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+				}
+				break
+			case 'MCQ':
+				html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+				html += `<li class="PersonInfoPanel">Please select one of the following options:</li>`
+				html += `<button class="PersonInfoPanel mcqOption" value="A">A</button>`
+				html += `<button class="PersonInfoPanel mcqOption" value="B">B</button>`
+				html += `<button class="PersonInfoPanel mcqOption" value="C">C</button>`
+				html += `<button class="PersonInfoPanel mcqOption" value="D">D</button>`
+				if (jsonObject.canBeSkipped) {
+					html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+				}
+				break
+			case 'TEXT':
+				html += `<li class="PersonInfoPanel">${jsonObject.questionText}</li>`
+				html += `<li class="PersonInfoPanel">Please enter a single word answer:</li>`
+				html += '<input class="PersonInfoPanel" id="PlayerAnswer" type="text"/>'
+				html +=
+					'<input class="PersonInfoPanel" id="SubmitButton" type="button" value="Submit" />'
+				if (jsonObject.canBeSkipped) {
+					html += `<button class="PersonInfoPanel skipButton" value="SKIP">SKIP</button>`
+				}
+				break
+			default:
+				break
+		}
 	}
-	html += `</div>`
-	return html
+	html += `</div>`;
+	return html;
 }
 
 
@@ -114,9 +116,9 @@ function submitAnswer(booleanAnswer = null, mcqAnswer = null, sessionID) {
 		.then(response => response.json())
 		.then(jsonObject => {
 			console.log(jsonObject)
-			if (jsonObject.status === 'OK') {
-				getQuestion(sessionID)
-			}
+				if (jsonObject.status === 'OK') {
+						getQuestion(sessionID)
+				}
 		})
 		.catch(error => {
 			console.error('Error submitting answer:', error)
