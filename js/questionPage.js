f2de690cf90ea6fdcc3a123c34d5d41f27ce90d9
Desire32 <nmarkov@uclan.ac.uk>
2024-03-25 13:46:51 +0200

    basic location api

--- js/questionPage.js
+++ js/questionPage.js
@@ -141,3 +141,21 @@ async function loadScore(sessionID) {
 start().then(sessionID => {
 	loadScore(sessionID)
 })
+
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition)
+	} else {
+		alert('Geolocation is not supported by your browser.')
+	}
+}
+function showPosition(position) {
+	alert(
+		'Latitude: ' +
+			position.coords.latitude +
+			', Longitude: ' +
+			position.coords.longitude
+	)
+}
+getLocation()
