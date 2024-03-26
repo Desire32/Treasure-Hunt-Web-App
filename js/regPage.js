096bf32664734218dc268484fd7597556538342c
Desire32 <nmarkov@uclan.ac.uk>
2024-03-26 17:36:38 +0200

    infinite alert fix

--- js/regPage.js
+++ js/regPage.js
@@ -20,75 +20,62 @@ function fetchTreasureHunts() {
 			elements.treasureHuntsListElement.innerHTML = html
 			elements.treasureHuntsListElement.addEventListener(
 				'click',
-				async function (event) {
+				function (event) {
 					let clickedElement = event.target
 					let uuid = clickedElement.getAttribute('data-uuid')
 					setCookie('uuid', uuid, 30)
 					if (uuid) {
 						elements.userInput.style.display = 'block'
 						elements.treasureHuntsListElement.style.display = 'none'
-						await start()
 					}
 				}
 			)
 		})
 }
 
-async function start() {
+elements.nameButton.addEventListener('click', async function () {
 	let uuid = getCookie('uuid')
 	let playerName = elements.playerNameInput.value.trim()
-	if (
-		playerName !== '' &&
-		!/^\d+$/.test(playerName) &&
-		playerName.length <= 50
-	) {
-		let storedWords = JSON.parse(getCookie('storedWords')) || []
-		if (storedWords.includes(playerName)) {
-			alert(
-				'This name has already been used before. Please enter a different name.'
-			)
-			return false
-		}
+	let storedWords = JSON.parse(getCookie('storedWords')) || []
 
-		let url =
-			'https://codecyprus.org/th/api/start' +
-			'?player=' +
-			playerName +
-			'&app=ComputerLegendsApp' +
-			'&treasure-hunt-id=' +
-			uuid
-
-		let response = await fetch(url)
-		let jsonObject = await response.json()
-		console.log(jsonObject)
-		let sessionID = jsonObject.session
-		setCookie('sessionID', sessionID, 30)
-		addWordToStorage()
-		getQuestion(sessionID)
-		return sessionID
-	} else {
-		if (/^\d+$/.test(playerName)) {
-			alert('Name cannot consist only of numbers.')
-		} else if (playerName.length > 50) {
-			alert('Name should not exceed 50 characters.')
-		} else {
-			alert('Please enter a valid name.')
-		}
-		return false
+	if (playerName === '') {
+		alert('Please enter a name.')
+		return
+	} else if (/^\d+$/.test(playerName)) {
+		alert('Name cannot consist only of numbers.')
+		return
+	} else if (playerName.length > 50) {
+		alert('Name should not exceed 50 characters.')
+		return
+	} else if (storedWords.includes(playerName)) {
+		alert(
+			'This name has already been used before. Please enter a different name.'
+		)
+		return
 	}
-}
 
-elements.nameButton.addEventListener('click', async function () {
-	let sessionID = await start()
+	let url =
+		'https://codecyprus.org/th/api/start' +
+		'?player=' +
+		playerName +
+		'&app=ComputerLegendsApp' +
+		'&treasure-hunt-id=' +
+		uuid
+
+	let response = await fetch(url)
+	let jsonObject = await response.json()
+	console.log(jsonObject)
+	let sessionID = jsonObject.session
+	setCookie('sessionID', sessionID, 30)
+	addWordToStorage()
+
 	if (sessionID) {
+		getQuestion(sessionID)
 		loadScore(sessionID)
 		document.getElementById('score').style.display = 'block'
 	}
 })
 
-start().then(sessionID => {
-	getQuestion(sessionID)
-})
 
 function addWordToStorage() {
 	let word = elements.playerNameInput.value.trim()
