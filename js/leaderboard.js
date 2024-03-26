ca4784ea7fcfa49d6093fd9f3954eba6fcca9147
Desire32 <nmarkov@uclan.ac.uk>
2024-03-26 19:51:15 +0200

    leaderboard fix

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,3 +1,34 @@
+// Define the createTable function
+function createTable() {
+	var numCols = 4
+	var numRows = 10
+	var tableHtml = "<table border='1'>"
+
+	// Creating headers
+
+	tableHtml += '<tr>'
+	tableHtml += '<th>Rank</th>'
+	tableHtml += '<th>Name</th>'
+	tableHtml += '<th>Points</th>'
+	tableHtml += '<th>Completion Time</th>'
+	tableHtml += '</tr>'
+
+	// Creating empty rows
+	for (var i = 0; i < numRows; i++) {
+		tableHtml += '<tr>'
+		for (var j = 0; j < numCols; j++) {
+			tableHtml += '<td></td>' // Empty cell
+		}
+		tableHtml += '</tr>'
+	}
+
+	tableHtml += '</table>'
+	document.getElementById('tableContainer').innerHTML = tableHtml
+}
+
+// Automatically call createTable when the page loads
+window.onload = createTable
+
 const nameTextElement = document.getElementById('nameText')
 const scoreFieldElement = document.getElementById('scoreField')
 getname()
@@ -11,8 +42,7 @@ function Rank(score, maxScore, time, totalTime) {
 }
 function getname() {
 	const URL =
-		'https://codecyprus.org/th/api/name?session=' +
-		getCookies(sessionID)
+		'https://codecyprus.org/th/api/name?session=' + getCookies(sessionID)
 	fetch(URL)
 		.then(response => response.json())
 		.then(jsonObject => {
