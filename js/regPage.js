61b52b3f155f031b2c9703c6152bfdf6ec92f52b
Desire32 <nmarkov@uclan.ac.uk>
2024-03-26 17:39:22 +0200

    Revert "infinite alert fix"
    This reverts commit 096bf32664734218dc268484fd7597556538342c.

--- js/regPage.js
+++ js/regPage.js
@@ -20,62 +20,75 @@ function fetchTreasureHunts() {
 			elements.treasureHuntsListElement.innerHTML = html
 			elements.treasureHuntsListElement.addEventListener(
 				'click',
-				function (event) {
+				async function (event) {
 					let clickedElement = event.target
 					let uuid = clickedElement.getAttribute('data-uuid')
 					setCookie('uuid', uuid, 30)
 					if (uuid) {
 						elements.userInput.style.display = 'block'
 						elements.treasureHuntsListElement.style.display = 'none'
+						await start()
 					}
 				}
 			)
 		})
 }
 
-elements.nameButton.addEventListener('click', async function () {
+async function start() {
 	let uuid = getCookie('uuid')
 	let playerName = elements.playerNameInput.value.trim()
-	let storedWords = JSON.parse(getCookie('storedWords')) || []
+	if (
+		playerName !== '' &&
+		!/^\d+$/.test(playerName) &&
+		playerName.length <= 50
+	) {
+		let storedWords = JSON.parse(getCookie('storedWords')) || []
+		if (storedWords.includes(playerName)) {
+			alert(
+				'This name has already been used before. Please enter a different name.'
+			)
+			return false
+		}
 
-	if (playerName === '') {
-		alert('Please enter a name.')
-		return
-	} else if (/^\d+$/.test(playerName)) {
-		alert('Name cannot consist only of numbers.')
-		return
-	} else if (playerName.length > 50) {
-		alert('Name should not exceed 50 characters.')
-		return
-	} else if (storedWords.includes(playerName)) {
-		alert(
-			'This name has already been used before. Please enter a different name.'
-		)
-		return
-	}
+		let url =
+			'https://codecyprus.org/th/api/start' +
+			'?player=' +
+			playerName +
+			'&app=ComputerLegendsApp' +
+			'&treasure-hunt-id=' +
+			uuid
 
-	let url =
-		'https://codecyprus.org/th/api/start' +
-		'?player=' +
-		playerName +
-		'&app=ComputerLegendsApp' +
-		'&treasure-hunt-id=' +
-		uuid
-
-	let response = await fetch(url)
-	let jsonObject = await response.json()
-	console.log(jsonObject)
-	let sessionID = jsonObject.session
-	setCookie('sessionID', sessionID, 30)
-	addWordToStorage()
-
-	if (sessionID) {
+		let response = await fetch(url)
+		let jsonObject = await response.json()
+		console.log(jsonObject)
+		let sessionID = jsonObject.session
+		setCookie('sessionID', sessionID, 30)
+		addWordToStorage()
 		getQuestion(sessionID)
+		return sessionID
+	} else {
+		if (/^\d+$/.test(playerName)) {
+			alert('Name cannot consist only of numbers.')
+		} else if (playerName.length > 50) {
+			alert('Name should not exceed 50 characters.')
+		} else {
+			alert('Please enter a valid name.')
+		}
+		return false
+	}
+}
+
+elements.nameButton.addEventListener('click', async function () {
+	let sessionID = await start()
+	if (sessionID) {
 		loadScore(sessionID)
 		document.getElementById('score').style.display = 'block'
 	}
 })
 
+start().then(sessionID => {
+	getQuestion(sessionID)
+})
 
 function addWordToStorage() {
 	let word = elements.playerNameInput.value.trim()
