38accd638d7e75da866a76084ed24393c1edae25
Desire32 <nmarkov@uclan.ac.uk>
2024-03-27 23:32:21 +0200

    burger menu small fix

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,4 +1,3 @@
-// code that reproduces so-called "burger-menu" on each of the pages, allowing you to click and select from a menu when the screen is reduced
 
 let burgerOpen = document.querySelector('.burger-menu-button')
 let burgerMenu = document.querySelector('.burger-menu')
@@ -15,13 +14,3 @@ burgerOpen.onclick = function () {
 	}
 }
 
-// a button that appears on the page "products", allowing you to return to the very top of the page
-
-let buttonPressed = document.getElementById('Button')
-
-buttonPressed.addEventListener('click', function () {
-	window.scrollTo({
-		top: 0,
-		behavior: 'smooth',
-	})
-})
