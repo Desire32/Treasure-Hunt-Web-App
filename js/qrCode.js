7cdfef8f41eed0b32690e0e5b9aaa99c906218ba
Desire32 <nmarkov@uclan.ac.uk>
2024-03-28 23:58:06 +0200

    debug

--- js/qrCode.js
+++ js/qrCode.js
@@ -74,7 +74,7 @@ scanner.addListener('scan', function (content) {
 })*/
 
 
-let scanner = new Instascan.Scanner({
+/*let scanner = new Instascan.Scanner({
 	video: document.getElementById('preview'),
 })
 scanner.addListener('scan', function (content) {
@@ -90,7 +90,39 @@ Instascan.Camera.getCameras()
 	})
 	.catch(function (e) {
 		console.error(e)
+	})*/
+
+
+
+  var opts = {
+		continuous: true,
+		video: document.getElementById('preview'),
+		captureImage: false,
+		backgroundScan: true,
+		refractoryPeriod: 5000,
+		scanPeriod: 1,
+	}
+
+	var scanner = new Instascan.Scanner(opts)
+
+	scanner.addListener('scan', function (content) {
+		console.log(content)
 	})
 
-
+	document
+		.getElementById('CameraButton')
+		.addEventListener('click', function () {
+			Instascan.Camera.getCameras()
+				.then(function (cameras) {
+					if (cameras.length > 0) {
+						scanner.start(cameras[0])
+					} else {
+						console.error('No cameras found.')
+					}
+				})
+				.catch(function (err) {
+					console.error('Error accessing camera:', err)
+					alert('Error accessing camera')
+				})
+		})
 
