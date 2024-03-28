f6236ed0675ab9444b22258dcc6dd60b30db0617
Desire32 <nmarkov@uclan.ac.uk>
2024-03-28 23:28:42 +0200

    debug with links

--- js/qrCode.js
+++ js/qrCode.js
@@ -71,23 +71,26 @@ document
 scanner.addListener('scan', function (content) {
 	console.log(content)
 	document.getElementById('content').innerHTML = content
+})*/
+
+
+let scanner = new Instascan.Scanner({
+	video: document.getElementById('preview'),
 })
-*/
-
-let scanner = new Instascan.scanner({video : document.getElementById('preview')})
-
-Instascan.Camera.getCameras().then(function(cameras){
-if(cameras.length > 0){
-  scanner.start(cameras[0])
-} else {
-  alert('No cameras found')
-}
-
-}).catch(function(e){
-  console.error(e)
+scanner.addListener('scan', function (content) {
+	console.log(content)
 })
+Instascan.Camera.getCameras()
+	.then(function (cameras) {
+		if (cameras.length > 0) {
+			scanner.start(cameras[0])
+		} else {
+			console.error('No cameras found.')
+		}
+	})
+	.catch(function (e) {
+		console.error(e)
+	})
 
-scanner.addListener('scan', function(c)
-{
-   document.getElementById('content').value = c
-})
+
+
