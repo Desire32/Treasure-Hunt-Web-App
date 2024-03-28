3b3fe9735221d1c2867ba60343e0565061c003c2
Desire32 <nmarkov@uclan.ac.uk>
2024-03-28 20:06:49 +0200

    treasure hunt fix with going back

--- js/regPage.js
+++ js/regPage.js
@@ -4,7 +4,7 @@ const elements = {
 	playerNameInput: document.getElementById('playerName'),
 	nameButton: document.getElementById('nameButton'),
 	userInput: document.querySelector('.userInput'),
-	scoreElement: document.getElementById('score')
+	scoreElement: document.getElementById('score'),
 }
 
 fetchTreasureHunts()
@@ -35,6 +35,7 @@ function fetchTreasureHunts() {
 								elements.treasureHuntsListElement.style.display = 'none'
 								sessionID = await start()
 								setCookie('sessionID', sessionID, 30)
+								history.pushState({ uuid: uuid }, '')
 							}
 						} else {
 							alert('This treasure hunt is unavailable')
@@ -46,6 +47,13 @@ function fetchTreasureHunts() {
 		getQuestion(sessionID)
 		loadScore(sessionID)
 		elements.scoreElement.style.display = 'block'
+	}
+}
+
+window.onpopstate = function (event) {
+	if (event.state && event.state.uuid) {
+		elements.userInput.style.display = 'none'
+		elements.treasureHuntsListElement.style.display = 'block'
 	}
 }
 
@@ -129,5 +137,3 @@ async function loadScore(sessionID) {
 
 	elements.scoreElement.textContent = `Score: ${jsonObject.score}`
 }
-
-
