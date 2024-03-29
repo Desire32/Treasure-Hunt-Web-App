1b947fe1171c841df80a56158a7f75ad2875a7f8
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 09:56:00 +0200

    fixed qr code

--- js/cookies.js
+++ js/cookies.js
@@ -79,8 +79,6 @@ async function getLocation() {
 	})
 }
 
-
-
 async function showPosition(position) {
 	setCookie('latitude', position.coords.latitude, 30)
 	setCookie('longitude', position.coords.longitude, 30)
--- js/questionPage.js
+++ js/questionPage.js
@@ -102,6 +102,8 @@ function generateQuestionHTML(jsonObject) {
 		var opts = {
 			continuous: true,
 			video: document.getElementById('preview'),
+			facingMode: 'environment',
+			mirror: false,
 			captureImage: false,
 			backgroundScan: true,
 			refractoryPeriod: 5000,
@@ -110,22 +112,36 @@ function generateQuestionHTML(jsonObject) {
 
 		var scanner = new Instascan.Scanner(opts)
 
-		document
-			.getElementById('CameraButton')
-			.addEventListener('click', function () {
+		let isActive = false
+
+		function switchCamera() {
+			if (isActive) {
+				scanner.stop()
+				document.getElementById('preview').style.display = 'none'
+				isActive = false
+				document.getElementById('CameraButton').textContent = 'Turn on camera'
+			} else {
+				//getLocation()
 				Instascan.Camera.getCameras()
 					.then(function (cameras) {
-						if (cameras.length > 0) {
-							scanner.start(cameras[0])
-						} else {
-							console.error('No cameras found.')
-							alert('No cameras found.')
+						let backCam = cameras.find(function (camera) {
+							return camera.name.indexOf('back') !== -1
+						})
+
+						if (backCam) {
+							scanner.start(backCam)
+							isActive = true
+							document.getElementById('preview').style.display = 'block'
+							document.getElementById('CameraButton').textContent =
+								'Turn off camera'
 						}
 					})
-					.catch(function (e) {
-						console.error(e)
-					})
-			})
+			}
+		}
+
+		document
+			.getElementById('CameraButton')
+			.addEventListener('click', switchCamera)
 
 		scanner.addListener('scan', function (content) {
 			alert(content)
