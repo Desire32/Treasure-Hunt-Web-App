6d3fd3b2f0d499b61b24e3bb5cbdf50e09ac60bd
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 06:08:21 +0200

    Update qrCode.js

--- js/qrCode.js
+++ js/qrCode.js
@@ -1,6 +1,45 @@
 
 
+
 /*var opts = {
+	continuous: true,
+	video: document.getElementById('preview'),
+	captureImage: false,
+	backgroundScan: true,
+	refractoryPeriod: 5000,
+	scanPeriod: 1,
+}
+
+var scanner = new Instascan.Scanner(opts)
+
+
+document.getElementById('CameraButton').addEventListener('click', function () {
+	Instascan.Camera.getCameras()
+		.then(function (cameras) {
+			if (cameras.length > 0) {
+				scanner.start(cameras[currentCameraIndex])
+			} else {
+				console.error('No cameras found.')
+				alert('No cameras found.')
+			}
+		})
+		.catch(function (e) {
+			console.error(e)
+		})
+})
+
+document
+	.getElementById('SwitchCameraButton')
+	.addEventListener('click', function () {
+		currentCameraIndex = (currentCameraIndex + 1) % cameras.length 
+		startCamera(currentCameraIndex)
+	})
+
+scanner.addListener('scan', function (content) {
+	alert(content)
+})*/
+
+var opts = {
 	continuous: true,
 	video: document.getElementById('preview'),
 	captureImage: false,
@@ -25,9 +64,11 @@ function stopCamera() {
 	}
 }
 
-document.getElementById('CameraButton').addEventListener('click', stopCamera)
+document
+	.getElementById('SwitchCameraButton')
+	.addEventListener('click', stopCamera)
 
-document.getElementById('button').addEventListener('click', function () {
+document.getElementById('CameraButton').addEventListener('click', function () {
 	Instascan.Camera.getCameras()
 		.then(function (cameras) {
 			if (cameras.length > 0) {
@@ -44,40 +85,7 @@ document.getElementById('button').addEventListener('click', function () {
 		})
 })
 
-
-})*/
-
-
-var currentCameraIndex = 0
-var cameras = []
-
-function startCamera(index) {
-	if (cameras.length > 0) {
-		scanner.start(cameras[index])
-	} else {
-		console.error('No cameras found.')
-		alert('No cameras found.')
-	}
-}
-
-document.getElementById('CameraButton').addEventListener('click', function () {
-	Instascan.Camera.getCameras()
-		.then(function (cams) {
-			cameras = cams
-			startCamera(currentCameraIndex)
-		})
-		.catch(function (e) {
-			console.error(e)
-		})
-})
-
-document
-	.getElementById('SwitchCameraButton')
-	.addEventListener('click', function () {
-		currentCameraIndex = (currentCameraIndex + 1) % cameras.length 
-		startCamera(currentCameraIndex)
-	})
-
 scanner.addListener('scan', function (content) {
-	alert(content)
+	console.log(content)
+	document.getElementById('content').innerHTML = content
 })
