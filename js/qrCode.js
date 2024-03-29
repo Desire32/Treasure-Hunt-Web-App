6a927d21c50969ae37d5973d44adeeb45aebbcf1
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 05:59:17 +0200

    debug change camera

--- js/qrCode.js
+++ js/qrCode.js
@@ -60,11 +60,14 @@ var opts = {
 var scanner = new Instascan.Scanner(opts)
 
 
+let currentCameraIndex = 0
+
 document.getElementById('CameraButton').addEventListener('click', function () {
 	Instascan.Camera.getCameras()
 		.then(function (cameras) {
 			if (cameras.length > 0) {
-				scanner.start(cameras[0])
+				currentCameraIndex = (currentCameraIndex + 1) % cameras.length 
+				scanner.start(cameras[currentCameraIndex])
 			} else {
 				console.error('No cameras found.')
 				alert('No cameras found.')
