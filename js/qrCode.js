d91ad67440e152f5bcbdc3c0183e49b9dbb52d5a
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 06:04:18 +0200

    debug switch button

--- js/qrCode.js
+++ js/qrCode.js
@@ -48,35 +48,35 @@ document.getElementById('button').addEventListener('click', function () {
 })*/
 
 
-var opts = {
-	continuous: true,
-	video: document.getElementById('preview'),
-	captureImage: false,
-	backgroundScan: true,
-	refractoryPeriod: 5000,
-	scanPeriod: 1,
+var currentCameraIndex = 0
+var cameras = []
+
+function startCamera(index) {
+	if (cameras.length > 0) {
+		scanner.start(cameras[index])
+	} else {
+		console.error('No cameras found.')
+		alert('No cameras found.')
+	}
 }
-
-var scanner = new Instascan.Scanner(opts)
-
-
-let currentCameraIndex = 0
 
 document.getElementById('CameraButton').addEventListener('click', function () {
 	Instascan.Camera.getCameras()
-		.then(function (cameras) {
-			if (cameras.length > 0) {
-				currentCameraIndex = (currentCameraIndex + 1) % cameras.length 
-				scanner.start(cameras[currentCameraIndex])
-			} else {
-				console.error('No cameras found.')
-				alert('No cameras found.')
-			}
+		.then(function (cams) {
+			cameras = cams
+			startCamera(currentCameraIndex)
 		})
 		.catch(function (e) {
 			console.error(e)
 		})
 })
+
+document
+	.getElementById('SwitchCameraButton')
+	.addEventListener('click', function () {
+		currentCameraIndex = (currentCameraIndex + 1) % cameras.length 
+		startCamera(currentCameraIndex)
+	})
 
 scanner.addListener('scan', function (content) {
 	alert(content)
