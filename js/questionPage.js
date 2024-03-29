35aa55cd8502f52871f705b1ad43537a69f2c648
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 03:46:33 +0200

    fix count question road map

--- js/questionPage.js
+++ js/questionPage.js
@@ -19,8 +19,8 @@ async function getQuestion(sessionID) {
 	}
 }
 
-function updateProgressBar(currentQuestionIndex) {
-	for (let i = 1; i <= 6; i++) {
+function updateProgressBar(currentQuestionIndex, jsonObject) {
+	for (let i = 1; i <= jsonObject.numOfQuestions; i++) {
 		
 		let point = document.getElementById(`point-${i}`)
 		
@@ -33,7 +33,9 @@ function updateProgressBar(currentQuestionIndex) {
 		point.innerText = i
 	}
 	let line = document.querySelector('.progress-bar-line')
-	line.style.width = `${(currentQuestionIndex / 6) * 50}%`
+	line.style.width = `${
+		(currentQuestionIndex / jsonObject.numOfQuestions) * 50
+	}%`
 	line.style.backgroundColor = 'gray'
 }
 
