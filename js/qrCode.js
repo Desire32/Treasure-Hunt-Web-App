555de19a49b9dc9464311dbecae7ea5c4deb60a8
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 05:26:14 +0200

    qr code debug

--- js/qrCode.js
+++ js/qrCode.js
@@ -1,55 +1,52 @@
+var opts = {
+	continuous: true,
+	video: document.getElementById('preview'),
+	captureImage: false,
+	backgroundScan: true,
+	refractoryPeriod: 5000,
+	scanPeriod: 1,
+}
 
-  var opts = {
-		continuous: true,
-		video: document.getElementById('preview'),
-		captureImage: false,
-		backgroundScan: true,
-		refractoryPeriod: 5000,
-		scanPeriod: 1,
-	}
+var scanner = new Instascan.Scanner(opts)
 
-	var scanner = new Instascan.Scanner(opts)
+document.getElementById('CameraButton').addEventListener('click', startCamera)
 
-
-	document
-		.getElementById('CameraButton')
-		.addEventListener('click', function () {
-			Instascan.Camera.getCameras()
-				.then(function (cameras) {
-					if (cameras.length > 0) {
-						stopCamera()
-						scanner.start(cameras[0])
-					} else {
-						console.error('No cameras found.')
-					}
-				})
-				.catch(function (err) {
-					console.error('Error accessing camera:', err)
-					alert('Error accessing camera')
-				})
-		})
-
-		document
-			.getElementById('StopCameraButton')
-			.addEventListener('click', stopCamera)
-
-
-		let isActive = false
-
-		function stopCamera() {
-			if (isActive) {
-				scanner.stop()
-				document.getElementById('preview').style.display = 'none'
-				isActive = false
-			} else {
-				scanner.start()
+function startCamera() {
+	Instascan.Camera.getCameras()
+		.then(function (cameras) {
+			if (cameras.length > 0) {
+				stopCamera()
+				scanner.start(cameras[0])
 				document.getElementById('preview').style.display = 'block'
 				isActive = true
+			} else {
+				console.error('No cameras found.')
 			}
-		}
-
-		scanner.addListener('scan', function (content) {
-			alert(content)
 		})
+		.catch(function (err) {
+			console.error('Error accessing camera:', err)
+			alert('Error accessing camera')
+		})
+}
 
+document
+	.getElementById('StopCameraButton')
+	.addEventListener('click', stopCamera)
 
+let isActive = false
+
+function stopCamera() {
+	if (isActive) {
+		scanner.stop()
+		document.getElementById('preview').style.display = 'none'
+		isActive = false
+	} else {
+		scanner.start()
+		document.getElementById('preview').style.display = 'block'
+		isActive = true
+	}
+}
+
+scanner.addListener('scan', function (content) {
+	alert(content)
+})
