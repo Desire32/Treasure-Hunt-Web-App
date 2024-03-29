724d1cc71bc1624e338db7cebb85574d56105d17
Desire32 <nmarkov@uclan.ac.uk>
2024-03-29 05:39:26 +0200

    qr cod debug

--- js/qrCode.js
+++ js/qrCode.js
@@ -1,4 +1,4 @@
-var opts = {
+/*var opts = {
 	continuous: true,
 	video: document.getElementById('preview'),
 	captureImage: false,
@@ -46,6 +46,54 @@ function stopCamera() {
 		isActive = true
 	}
 }
+
+scanner.addListener('scan', function (content) {
+	alert(content)
+})*/
+
+var opts = {
+	continuous: true,
+	video: document.getElementById('preview'),
+	captureImage: false,
+	backgroundScan: true,
+	refractoryPeriod: 5000,
+	scanPeriod: 1,
+}
+
+var scanner = new Instascan.Scanner(opts)
+
+let isActive = false
+
+function stopCamera() {
+	if (isActive) {
+		scanner.stop()
+		document.getElementById('preview').style.display = 'none'
+		isActive = false
+	} else {
+		scanner.start()
+		document.getElementById('preview').style.display = 'block'
+		isActive = true
+	}
+}
+
+document.getElementById('CameraButton').addEventListener('click', stopCamera)
+
+document.getElementById('button').addEventListener('click', function () {
+	Instascan.Camera.getCameras()
+		.then(function (cameras) {
+			if (cameras.length > 0) {
+				scanner.start(cameras[0])
+				isActive = true
+				document.getElementById('preview').style.display = 'block'
+			} else {
+				console.error('No cameras found.')
+				alert('No cameras found.')
+			}
+		})
+		.catch(function (e) {
+			console.error(e)
+		})
+})
 
 scanner.addListener('scan', function (content) {
 	alert(content)
